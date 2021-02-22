d634e2bffdbf731a37406f4e51a54533749b624b
Vincenzo Francavilla <vincenzo.francavilla@mt-vincenzof-linux.localspace>
2021-02-22 15:01:26 +0100

    format files

--- src/connectionPool.js
+++ src/connectionPool.js
@@ -1,32 +1,29 @@
 const mysql = require('mysql2'); //https://www.npmjs.com/package/mysql2
 const pool = mysql.createPool({
-	host     : process.env.DB_HOST,
-	user     : process.env.DB_USER,
-	database : process.env.DB_DATABASE,
-	password : process.env.DB_PASSWD,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	database: process.env.DB_DATABASE,
+	password: process.env.DB_PASSWD,
 	waitForConnections: true,
 	connectionLimit: 10,
-	queueLimit: 0
+	queueLimit: 0,
 });
-exports.lambdaHandler = async(event, context, callback) => {
+exports.lambdaHandler = async (event, context, callback) => {
 	context.callbackWaitsForEmptyEventLoop = false;
-	pool.getConnection(function(err, connection) {
-		if(err){
+	pool.getConnection(function (err, connection) {
+		if (err) {
 			console.log('error');
-		}else{
+		} else {
 			console.log('start connection');
-			connection.query(
-				'SELECT * FROM tasks',
-				function(err, rows, fields) {
-				if(err){
+			connection.query('SELECT * FROM tasks', function (err, rows, fields) {
+				if (err) {
 					console.log('error');
 				} else {
 					console.log('Connected!');
 					console.log(rows);
-				} 
 				}
-			);
+			});
 			pool.releaseConnection(connection);
 		}
-	 });
+	});
 };
